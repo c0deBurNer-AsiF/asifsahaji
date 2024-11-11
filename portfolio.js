9c84bc2db41a578ab0309c80bee6f78a39aa2d74
Asif Ali Sahaji <asifcoding0120@gmail.com>
2024-11-12 02:41:52 +0530

    Animation_1.O

--- portfolio.js
+++ portfolio.js
@@ -152,6 +152,29 @@ document.getElementById("btn1").addEventListener("click", function(){
 
 
 
+document.addEventListener("DOMContentLoaded", function () {
+    const options = { threshold: 0.1 };
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("fade-in");
+          observer.unobserve(entry.target);
+        }
+      });
+    }, options);
+  
+    document.querySelectorAll(".observed").forEach(section => {
+      observer.observe(section);
+    });
+  });
+  
+  
+  
+  
+
+
+
+
 
 
 
